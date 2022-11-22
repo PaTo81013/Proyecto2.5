2805623fd11c3c2adc5c851109630eaa7a5b8ec1
PaTo81013 <94476998+PaTo81013@users.noreply.github.com>
2022-11-22 08:46:50 -0600

    Particulas de explosion (#6)
    * Create the first part of the particles
    
    * Effect has been complete
    
    * Fix script format

--- Proyecto2.5/Assets/scripts/DestroyPlatform.cs
+++ Proyecto2.5/Assets/scripts/DestroyPlatform.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class DestroyPlatform : MonoBehaviour
 {
+    [SerializeField] public GameObject _effect = null;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("ball"))
         {
+            var newEffect = Instantiate(_effect, transform.position, Quaternion.identity);
+            Destroy(newEffect, 2);
             Destroy(other.gameObject);
             Destroy(gameObject);
         }
