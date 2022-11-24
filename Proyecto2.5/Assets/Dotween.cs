644c879db96b98dea59f29185e9a8c2159292882
PaTo81013 <94476998+PaTo81013@users.noreply.github.com>
2022-11-23 21:15:11 -0600

    Dootween finished

---
+++ Proyecto2.5/Assets/Dotween.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Dotween : MonoBehaviour
+{
+    void Start()
+    {
+        DOTween.Init();
+
+         
+    }
+}
