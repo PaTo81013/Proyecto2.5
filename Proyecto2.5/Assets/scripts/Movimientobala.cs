a4490df5c292a6967f6d8a05d46c1ac60063698a
PaTo81013 <94476998+PaTo81013@users.noreply.github.com>
2022-11-23 22:13:33 -0600

    Finished Audio

--- Proyecto2.5/Assets/scripts/Movimientobala.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-public class Movimientobala : MonoBehaviour
-{
-    [SerializeField] private float velocidad=4f;
-    void Update()
-    {
-        transform.Translate(Vector3.up*velocidad*Time.deltaTime);  
-    }
-}
