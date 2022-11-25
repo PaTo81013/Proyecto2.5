75b9d158c06f9d3486f3e461cd662d4e681aebd9
PaTo81013 <94476998+PaTo81013@users.noreply.github.com>
2022-11-25 15:37:54 -0600

    Dootween finished (#7)

--- Proyecto2.5/Assets/scripts/Shoot.cs
+++ Proyecto2.5/Assets/scripts/Shoot.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using UnityEngine;
-
+using DG.Tweening;
 public class Shoot : MonoBehaviour
 {
     [SerializeField] private GameObject _bullets = null;
     [SerializeField] private Transform _direction = null;
     [SerializeField] private float _amountbullets = 10f;
-
+    [SerializeField] private float _scaleobject = 1.62f;
+    [SerializeField] private float _cooldown;
+    [SerializeField] private bool _shootornot;
+    [SerializeField] private AudioClip _shoot;
     public void Shoots()
     {
-        if (_amountbullets > 0)
+        if (_shootornot)
         {
+            var sequence = DOTween.Sequence();
+            sequence.Append(transform.DOPunchScale(new Vector3(0, -1f, 0), 1, 1));
+            // sequence.AppendCallback(ChangeScale);
+            transform.localScale = new Vector3(_scaleobject, _scaleobject, 0);
+            AudioManager.instance.PlaySFX(_shoot);
             Instantiate(_bullets, _direction.position, Quaternion.identity);
-            _amountbullets--;
+            _shootornot = false;
         }
         else
         {
@@ -22,7 +30,15 @@ public class Shoot : MonoBehaviour
 
     private IEnumerator Reload()
     {
-        yield return new WaitForSeconds(2);
-        _amountbullets = 10;
+        yield return new WaitForSeconds(1);
+        _shootornot = true;
+    }
+
+    private void ChangeScale()
+    {
+        if (transform.localScale.y < 1.62)
+        {
+            transform.localScale = new Vector3(0, _scaleobject, 0);
+        }
     }
 }
