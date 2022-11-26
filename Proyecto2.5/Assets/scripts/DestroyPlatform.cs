a2415e8e7ca63d7480c47b71a4c102072a5a6472
PaTo81013 <94476998+PaTo81013@users.noreply.github.com>
2022-11-26 17:24:09 -0600

    Destroy sound
    Destroy sound will activate when the bullet collision with the platform.

--- Proyecto2.5/Assets/scripts/DestroyPlatform.cs
+++ Proyecto2.5/Assets/scripts/DestroyPlatform.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class DestroyPlatform : MonoBehaviour
 {
     [SerializeField] public GameObject _effect = null;
+    [SerializeField] private AudioClip _destroySound;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("ball"))
@@ -10,6 +11,7 @@ public class DestroyPlatform : MonoBehaviour
             var newEffect = Instantiate(_effect, transform.position, Quaternion.identity);
             Destroy(newEffect, 2);
             Destroy(other.gameObject);
+            AudioManager.instance.PlaySFX(_destroySound);
             Destroy(gameObject);
         }
     }
